260789e83c9423d9e643cd891e3c3a6909e32af5
Juanmunoz8 <mun231047@uvg.edu.gt>
2024-01-31 07:42:28 -0600

    CalculadoraPOSTFIX

--- CalculadoraPOSTFIX.java
+++ CalculadoraPOSTFIX.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class PostfixEvaluator {
+public class CalculadoraPOSTFIX {
     public static void main(String[] args) {
         try {
             File file = new File("datos.txt");
